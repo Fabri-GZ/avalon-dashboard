2bbe3117d0826779b56e9d7e43349c52e1b5f583
Fabrizio <fabriziovguzman@gmail.com>
2025-11-20 15:19:30 -0300

    Modified SignInWithGoogleButton style

--- src/app/(auth)/login/components/SignInWithGoogleButton.jsx
+++ src/app/(auth)/login/components/SignInWithGoogleButton.jsx
@@ -5,12 +5,18 @@ import { signInWithGoogle } from "../../../../../lib/auth-actions";
 const SignInWithGoogleButton = () => {
     return (
         <button
-        type="button"
-        className="w-full"
-        onClick={() => {
-            signInWithGoogle();
-        }}
-        >Login With Google</button>
+          type="button"
+          onClick={() => { signInWithGoogle();
+          }}
+          className="w-full flex items-center justify-center gap-2 border border-gray-300 bg-white hover:bg-gray-100 text-gray-700 font-medium py-2 rounded-lg transition-all duration-200 ease-in"
+        >
+          <img
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            alt="Google logo"
+            className="w-5 h-5"
+          />
+          Registrarse con Google
+        </button>
     );
 };
 
